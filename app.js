4a0643cf7ec3e5a318fa15355e7f29f7df38d5dc
Demzito <demazureetienne@gmail.com>
2021-01-13 19:05:46 +0100

    backend

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+//**ajouter l'adresse ip manuellement sur mongoDB Atlas en cas de problème */
+mongoose.connect('mongodb+srv://test:290296@p6.argo7.mongodb.net/P6?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname,'images')));
+
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+// npm install --save bcrypt
+// telecharger bcrypt pour crypter le mot de passe
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)  // on demande a hasher le password dans le body de la requete
+    .then(hash => { // retourne une promise , où l'on crée un nouvel utilisateur
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save() // sauvegarde de l'utlisateur dans la base de donnée
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email }) // findOne cherche un utilisateur par rapport a son mail
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password) // on compare le mdp entré et celui dans le body de la requete
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+                { userId : user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
